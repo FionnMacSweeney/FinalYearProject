28b7e58f97db0966372eee9323c49da3704309d9
FionnMacSweeney <FionnMacsweeney01@gmail.com>
2023-11-13 23:01:52 +0000

    css for home page

--- Formula1-Race-Strategy-Sim/src/app/home/home.page.ts
+++ Formula1-Race-Strategy-Sim/src/app/home/home.page.ts
@@ -2,22 +2,35 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { Router } from '@angular/router';
 
+interface Constructor {
+  constructorId: string;
+  name: string;
+  nationality: string;
+  imageUrl?: string;
+  // other properties that your constructor objects have
+}
+
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-  constructors: any[] = [];
+  constructors: Constructor[] = []; // Use the Constructor interface
 
   constructor(private dataService: DataService, private router: Router) {}
 
   ngOnInit() {
-    this.dataService.getConstructors().subscribe(data => {
-      this.constructors = data;
-      console.log('Constructors:', this.constructors); // Check the structure of constructors array
+    this.dataService.getConstructors().subscribe((data: Constructor[]) => {
+      this.constructors = data.map((constructor: Constructor) => ({
+        ...constructor,
+        imageUrl: `./assets/images/${constructor.constructorId}.png`
+      }));
+      console.log('Constructors with images:', this.constructors);
     });
   }
+
   
 
   
